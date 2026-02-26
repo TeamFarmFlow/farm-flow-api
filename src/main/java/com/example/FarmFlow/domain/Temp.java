890383ebc1262cec435d1d2461b8bda3ee5691fd
최원영 <choewy32@gmail.com>
2026-02-26 22:43:38 +0900

    chore: checkout

---
+++ src/main/java/com/example/FarmFlow/domain/Temp.java
@@ -0,0 +1,15 @@
+package com.example.FarmFlow.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Temp {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+}
