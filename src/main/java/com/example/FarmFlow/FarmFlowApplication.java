4536c9d55e39c061da329d2074e8fe7a2e3ab641
최원영 <choewy32@gmail.com>
2026-02-26 22:04:16 +0900

    chore: rename

--- src/main/java/com/example/FarmFlow/FarmFlowApplication.java
+++ src/main/java/com/example/FarmFlow/FarmFlowApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.FarmFlow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class FarmFlowApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(FarmFlowApplication.class, args);
 	}
 
 }
